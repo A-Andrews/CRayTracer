8be23997ae59477e3acc7faa22311c6ffa0e6b09
A-Andrews <44030780+A-Andrews@users.noreply.github.com>
2026-07-01 15:27:18 +0100

    Add gamma 2 correction

--- include/colour.h
+++ include/colour.h
@@ -4,14 +4,25 @@
 #include "interval.h"
 #include "vec3.h"
 
+#include <math.h>
 #include <stdio.h>
 
 typedef Vec3 Colour;
+
+static inline double linear_to_gamma(double linear_component) {
+  if (linear_component > 0)
+    return sqrt(linear_component);
+  return 0;
+}
 
 void write_colour(FILE *file, const Colour pixel_colour) {
   double r = pixel_colour.x;
   double g = pixel_colour.y;
   double b = pixel_colour.z;
+
+  r = linear_to_gamma(r);
+  g = linear_to_gamma(g);
+  b = linear_to_gamma(b);
 
   // Translate the [0,1] component values to the byte range [0,255].
   static const Interval intensity = (Interval){0.000, 0.999};
