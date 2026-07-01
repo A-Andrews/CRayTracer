88c3d554cea3f1870739282daa4bb635916eaa92
A-Andrews <44030780+A-Andrews@users.noreply.github.com>
2026-07-01 17:56:15 +0100

    Fix reflaction by updating camera logic

--- include/camera.h
+++ include/camera.h
@@ -29,10 +29,11 @@ Colour camera_ray_colour(const Ray r, const HittableList *world, int depth) {
     return (Colour){0, 0, 0};
 
   if (hit_list(world, r, (Interval){0.001, INFINITY}, &rec)) {
-    Vec3 direction = vec3_random_on_hemisphere(
-        vec3_add(rec.normal, vec3_random_unit_vector()));
-    return vec3_scale(
-        0.5, camera_ray_colour((Ray){rec.p, direction}, world, depth - 1));
+    Ray scattered;
+    Colour attenuation;
+    if (scatter(&r, &rec, &attenuation, &scattered))
+      return vec3_element_wise_product(
+          attenuation, camera_ray_colour(scattered, world, depth - 1));
   }
 
   Vec3 unit_direction = vec3_unit_vector(r.direction);
